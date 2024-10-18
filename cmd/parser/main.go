af57f4ef70f8e6875b7b8200b7b26bc1e64b1634
Liam Keene <liam.keene@gmail.com>
2024-10-18 10:03:10 +0900

    CLI arg parsing

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -1,2 +1,40 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	outputFlag := flag.String("output", "stdout", "Output destination: stdout, json, or html")
+	logDirFlag := flag.String("logdir", "./logs", "Directory containing the nginx logs to parse")
+	persistFlag := flag.String("persist", "", "Persist parsed logs to DB file (SQLite)")
+
+	flag.Parse()
+
+	if _, err := os.Stat(*logDirFlag); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "Error: log directory %s does not exist\n", *logDirFlag)
+		os.Exit(1)
+	}
+
+	fmt.Println("Generating report ...")
+
+	switch *outputFlag {
+	case "stdout":
+        fmt.Println("Write to stdout")
+	case "json":
+        fmt.Println("Write to JSON file")
+	case "html":
+        fmt.Println("Write to HTML file")
+	default:
+		fmt.Fprintf(os.Stderr, "Error: invalid output option %s\n", *outputFlag)
+		os.Exit(1)
+	}
+
+	if *persistFlag != "" {
+        fmt.Println("Save data to SQLite")
+	}
+
+	fmt.Println("Report complete!")
+}
